09cf9d68c89ca701dd0fedbb20afaf851f504078
oldamazingbrain@gmail.com <oldamazingbrain@gmail.com>
2020-09-26 14:11:46 -0400

    i lied but it works now

--- Driver.py
+++ Driver.py
@@ -7,13 +7,13 @@ def read_file(filename) -> list:
         to_add = line.split(",")
         for i in range(0, len(to_add)):
             to_add[i] = to_add[i].strip()
-            to_return.append(to_add)
+        to_return.append(to_add)
     return to_return
 
 
 def main():
     big_list = read_file('cali_cars.csv')
-    print(big_list)
+    #print(big_list)
 
 
 if __name__ == "__main__":
