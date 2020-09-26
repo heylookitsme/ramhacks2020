2747f27eb4771ebb4354efa4a8927beaf8392bee
rei <heylookitsme@github.com>
2020-09-26 17:16:54 -0400

    made gui *even better*

--- gui.py
+++ gui.py
@@ -9,24 +9,37 @@ class GUI:
         
         #title 
         self.label = Label(master, text="Cars, better than trains")
-        self.label.pack()
-
+        self.label.pack(padx=10, pady=10)
+        
         #menu to select location 
         self.mb = Menubutton(master, text="My Location")
         self.mb.menu = Menu(self.mb)
         self.mb["menu"]=self.mb.menu
 
         a = IntVar()
-        self.mb.menu.add_checkbutton(label = "hng", variable =a)
-        self.mb.pack(side=LEFT)
-        
+        self.mb.menu.add_checkbutton(label = "location", variable =a)
+        self.mb.pack(side=LEFT, padx=10, pady=10)
+
+
+        #menu to car 
+        self.mb = Menubutton(master, text="Car I want to Buy ")
+        self.mb.menu = Menu(self.mb)
+        self.mb["menu"]=self.mb.menu
+
+        a = IntVar()
+        self.mb.menu.add_checkbutton(label = "car", variable =a)
+        self.mb.pack(side=LEFT, padx=10, pady=10)
+
+
         #map 
-        self.map = Canvas(master, bg="white", height=300, width=1000)
-        self.map.pack(side=RIGHT)
+        self.map = Canvas(master, bg="white", height=300, width=500)
+        self.map.pack(side=RIGHT, padx=10, pady=10)
+        self.USmap = PhotoImage(file="map.png")
+        self.map.create_image((0,0),image=self.USmap,anchor='nw')
 
         #close button 
         self.close_button = Button(master, text="Close", command=master.quit)
-        self.close_button.pack(side=LEFT)
+        self.close_button.pack(side=LEFT, padx=10, pady=10)
 
     def greet(self):
         print("Greetings!")
