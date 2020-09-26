5245395879d0575669e44f76b6a31f322ea0dd03
oldamazingbrain@gmail.com <oldamazingbrain@gmail.com>
2020-09-26 14:09:33 -0400

    ok it actually works now no bamboozle

--- Driver.py
+++ Driver.py
@@ -1,17 +1,19 @@
 def read_file(filename) -> list:
     to_return = []  # list of lists to be returned
     f = open(filename)  # default reading/text
-    try:
-        s = f.readline()
-        to_add = s.split(',')
+    lines = f.readlines()
+
+    for line in lines:
+        to_add = line.split(",")
         for i in range(0, len(to_add)):
             to_add[i] = to_add[i].strip()
-    finally:
-        return to_return
+            to_return.append(to_add)
+    return to_return
 
 
 def main():
-    print("Hello World")
+    big_list = read_file('cali_cars.csv')
+    print(big_list)
 
 
 if __name__ == "__main__":
