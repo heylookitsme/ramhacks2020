e58f6013100f55c6bc675e5b0a803c06b8a0ce76
rei <heylookitsme@github.com>
2020-09-26 16:52:03 -0400

    gui looks okay

--- gui.py
+++ gui.py
@@ -1,25 +1,39 @@
-from tkinter import Tk, Label, Button
+from tkinter import *
+from tkinter.ttk import *
+from ttkthemes import ThemedTk
 
-class MyFirstGUI:
+class GUI:
     def __init__(self, master):
         self.master = master
-        master.title("A simple GUI")
-
-        self.label = Label(master, text="This is our first GUI!")
+        master.title("Mr Worldwide but its Cars")
+        
+        #title 
+        self.label = Label(master, text="Cars, better than trains")
         self.label.pack()
 
-        self.greet_button = Button(master, text="Greet", command=self.greet)
-        self.greet_button.pack()
+        #menu to select location 
+        self.mb = Menubutton(master, text="My Location")
+        self.mb.menu = Menu(self.mb)
+        self.mb["menu"]=self.mb.menu
 
+        a = IntVar()
+        self.mb.menu.add_checkbutton(label = "hng", variable =a)
+        self.mb.pack(side=LEFT)
+        
+        #map 
+        self.map = Canvas(master, bg="white", height=300, width=1000)
+        self.map.pack(side=RIGHT)
+
+        #close button 
         self.close_button = Button(master, text="Close", command=master.quit)
-        self.close_button.pack()
+        self.close_button.pack(side=LEFT)
 
     def greet(self):
         print("Greetings!")
 
 def start():
-    root = Tk()
-    my_gui = MyFirstGUI(root)
+    root = ThemedTk(theme="equilux")
+    my_gui = GUI(root)
     root.mainloop()
 
 
