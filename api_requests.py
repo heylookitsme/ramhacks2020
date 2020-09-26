7b0521011f8de07089e3affcf499ab25db0db296
Strequals <andrewzhang1101@gmail.com>
2020-09-26 19:56:08 -0400

    halp

---
+++ api_requests.py
@@ -0,0 +1,40 @@
+import urllib.request
+import urllib.parse
+
+url="https://www.carmax.com/cars/api/search/run"
+user_agent='Mozilla/5.0 (X11; Linux x86_64; rv:81.0) Gecko/20100101 Firefox/81.0'
+
+def open_request(req):
+    response = None
+    with urllib.request.urlopen(req) as resp:
+        response = resp.read()
+    return response
+
+def car_request(uri,radius,zip):
+    values = {}
+    values['uri']=uri
+    values['skip']=0
+    values['take']=24
+    values['radius']=radius
+    values['zipCode']=zip
+    values['sort']=14
+    data = urllib.parse.urlencode(values)
+    
+    headers = {'User-Agent' : user_agent,
+        'Host' : 'www.carmax.com',
+        'Accept' : '*/*;q=0.8',
+        'Accept-Language' : 'en-US,en;q=0.5',
+        'Accept-Encoding' : 'gzip, deflate, br',
+        'Content-Type' : 'application/json',
+        'DNT' : 1,
+        'Connection' : 'keep-alive',
+        'Upgrade-Insecure-Requests' : 1}
+    
+    full_url = url + "?" + data
+    print(full_url)
+    req = urllib.request.Request(full_url,data=None,headers=headers)
+    print(req.type)
+    return open_request(req)
+    
+
+
