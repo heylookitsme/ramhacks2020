8e229bac23c12e8ba69c04a969ed488f6dcdbefa
oldamazingbrain@gmail.com <oldamazingbrain@gmail.com>
2020-09-26 13:35:21 -0400

    added reading file into 2d array

--- Driver.py
+++ Driver.py
@@ -1,5 +1,21 @@
+def read_file(filename) -> list:
+    to_return = []  # list of lists to be returned
+    try:
+        f = open(filename)  # default reading/text
+    except IOError:
+        print("IOError has occurred, check filename?")
+    try:
+        s = f.readline()
+        to_add = s.split(',')
+        for i in range(0, len(to_add)):
+            to_add[i] = to_add[i].strip()
+    finally:
+        return to_return
+
+
 def main():
-   
+    print("Hello World")
+
 
 if __name__ == "__main__":
-   main()
+    main()
