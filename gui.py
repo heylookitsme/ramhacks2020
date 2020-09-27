65ecfdf41f36f3aa75b5c373cb4a064fc284d0c0
rei <heylookitsme@github.com>
2020-09-27 06:23:23 -0400

    image

--- gui.py
+++ gui.py
@@ -222,6 +222,12 @@ class GUI:
         self.map.create_image((0,0),image=self.USmap,anchor='nw')
         self.state_from = None
         self.state_to = None
+
+        #graph visual
+        self.graf = Canvas(master, bg="white", height=322, width=627)
+        self.graf.pack(side=TOP, padx=10, pady=10)
+        self.linreg = PhotoImage(file="reg_display.png")
+        self.graf.create_image((0,0),image=self.linreg,anchor='nw')
                 
         #close button 
         self.close_button = Button(self.fright, text="Close", command=master.quit)
